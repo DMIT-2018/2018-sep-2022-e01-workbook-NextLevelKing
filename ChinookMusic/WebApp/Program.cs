6ce94c1b2865b3cd4b0364283ebb3b0ea8bfb3cc
NextLevelKing <90404552+NextLevelKing@users.noreply.github.com>
2022-11-02 20:42:43 -0600

    Setting up web page

--- ChinookMusic/WebApp/Program.cs
+++ ChinookMusic/WebApp/Program.cs
@@ -5,9 +5,29 @@ using WebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//supplied database connection due to the fact that we created this web app to use individual accounts
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Add another GetConnectionString to reference our database connection string
+
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+
+//given for the db connection to Defaultconnection string
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//code the db connection to the application DB context for Chinook
+//the implementation of the connect AND registration of the ChinooSystem services will be done in the ChinookSystem class library
+//to accomplish this task,
+//we will be using an "extension" method will extend the IServiceCollection Class
+//the extension method requires a parameter options.UseSqlServer(xxx)
+//where xxx is the connection string variable.
+//builder.Services.ChinookSystemBackedDependencies(options => options.UseSqlServer(connectionStringChinook));
+
+
+ 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
